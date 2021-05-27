401d8d544599b942157caed80d9f70e47a3adcb6
oreste-abizera <oresteabizera11@gmail.com>
2021-05-27 16:56:52 +0200

    Error return

--- src/utils/Error.ts
+++ src/utils/Error.ts
@@ -1,17 +1,17 @@
 interface IError {
-    error:string;
+  error: string;
 }
 
 /**
  * Throw error when something goes wrong
  * @param input value is an invalid input privided by user in function
  * @param requiredType required type of value to be inputed
- * @returns {IError}
+ * @returns {string}
  */
-function throwError(input:string,requiredType:string = 'string') : IError{
-    const error : IError =  {
-        error:`Invalid parameter type. \n '${input}' must be type '${requiredType}'`
-    }
-    console.error(error)
-    return error;
+function throwError(input: string, requiredType: string = 'string'): string {
+  const error: IError = {
+    error: `Invalid parameter type. \n '${input}' must be type '${requiredType}'`,
+  };
+  console.error(error);
+  return error.error;
 }
