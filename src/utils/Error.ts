fcbec4b95a6641097d3467d80f214780169c7076
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-05-27 12:13:55 +0200

    feat: docs

--- src/utils/Error.ts
+++ src/utils/Error.ts
@@ -2,6 +2,12 @@ interface IError {
     error:string;
 }
 
+/**
+ * Throw error when something goes wrong
+ * @param input value is an invalid input privided by user in function
+ * @param requiredType required type of value to be inputed
+ * @returns {IError}
+ */
 function ThrowError(input:string,requiredType:string = 'string') : IError{
     const error : IError =  {
         error:`Invalid parameter type. \n '${input}' must be type '${requiredType}'`
